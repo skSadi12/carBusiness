6cc2e3365b8519bcab225952dac528ac70dcc1b8
skSadi12 <sksadi433@gmail.com>
2025-05-15 20:11:12 +0600

    starting

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
