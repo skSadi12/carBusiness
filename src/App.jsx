f003693395ce187d04cba9594417f0dbcbc73c9f
skSadi12 <sksadi433@gmail.com>
2025-05-21 03:44:56 +0600

    Questions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Hero from './components/home/Hero'
 import Partners from './components/home/Partners'
 import Problem from './components/home/Problem'
+import Questions from './components/home/Questions'
 import Reviews from './components/home/Reviews'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Problem/>
       <Reviews/>
       <Partners/>
+      <Questions/>
     </main>
   )
 }
---
+++ src/components/home/Questions.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Gradient from "../reUse/Gradient";
+
+function Questions() {
+  return (
+    <section className='bg-[url("/faq-bg-line.svg")] pt-10 bg-no-repeat min-h-screen'>
+      <h2 className="text-[46px] font-bold text-center ">
+        Frequently <Gradient>Asked Questions</Gradient>
+      </h2>
+      <div className="max-w-[1200px]  mx-auto p-5 space-y-8">
+        <div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2-content"
+              id="panel2-header"
+            >
+              <Typography component="span" sx={{fontSize:'22px', fontWeight:"semibold"}}>Is my data secure?</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography sx={{fontSize:'12px',}}>
+                We cover a wide range of documents, including contracts, NDAs,
+                compliance reports, and more.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2-content"
+              id="panel2-header"
+            >
+              <Typography component="span" sx={{fontSize:'22px', fontWeight:"semibold"}}>
+                What types of documents can Ai GoverningDocs generate?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography sx={{fontSize:'12px',}}>
+                We cover a wide range of documents, including contracts, NDAs,
+                compliance reports, and more.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2-content"
+              id="panel2-header"
+            >
+              <Typography component="span" sx={{fontSize:'20px', fontWeight:"semibold"}}>
+                Can I customize the documents?
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography sx={{fontSize:'12px',}}>
+                We cover a wide range of documents, including contracts, NDAs,
+                compliance reports, and more.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Questions;
